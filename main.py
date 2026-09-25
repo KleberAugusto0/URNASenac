8d42fe6a8bc77e7f21a6a60c6826dbabce9a8ef5
JoaoCerbino <jonguicerbinonho@gmail.com>
2026-09-25 15:31:02 -0400

    Botões ja colocados

--- main.py
+++ main.py
@@ -8,15 +8,30 @@ class Menu_urna(QWidget):
         self.setWindowTitle("Menu")
         layout = QVBoxLayout()
         self.setLayout(layout)
+        
+
+        verificacao_zerezima = False
+
+        layout.addStretch(1)
+        txt_urna = QLabel("Urna Eletrónica")
+        layout.addWidget(txt_urna)
+        txt_urna.setAlignment(Qt.AlignmentFlag.AlignCenter) 
 
         btn_zerezima = QPushButton("Rélatorio inicial (Zerézima)")
-        layout.addWidget(btn_zerezima)
+        layout.addWidget(btn_zerezima,alignment=Qt.AlignmentFlag.AlignCenter)
+        btn_zerezima.setFixedSize(250,50)
+
         btn_votar = QPushButton("Votar")
-        layout.addWidget(btn_votar)
+        layout.addWidget(btn_votar,alignment=Qt.AlignmentFlag.AlignCenter)
+    
+
         btn_relatorio = QPushButton("Relatório Final")
-        layout.addWidget(btn_relatorio)
+        layout.addWidget(btn_relatorio,alignment=Qt.AlignmentFlag.AlignCenter)
+
+
         btn_sair = QPushButton("Sair")
-        layout.addWidget(btn_sair)
+        layout.addWidget(btn_sair,alignment=Qt.AlignmentFlag.AlignCenter) 
+        layout.addStretch(1)
 
 
 
