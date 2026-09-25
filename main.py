002f0e3f3b9189f01870d8ded0ac5bd1bfde1992
JoaoCerbino <jonguicerbinonho@gmail.com>
2026-09-25 15:05:23 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from PySide6.QtWidgets import QLabel, QVBoxLayout,QPushButton,QWidget,QApplication,QFrame
+from PySide6.QtCore import Qt
+import sys,os
+
+class Menu_urna(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Menu")
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        btn_zerezima
