3b25f53da0f985ba859dba8d7b998c659b92425d
JoaoCerbino <jonguicerbinonho@gmail.com>
2026-09-25 15:15:15 -0400

    commit pra puxa arquivo da main

--- main.py
+++ main.py
@@ -6,7 +6,22 @@ class Menu_urna(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Menu")
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
+        layout = QVBoxLayout()
+        self.setLayout(layout)
 
-        btn_zerezima
+        btn_zerezima = QPushButton("Rélatorio inicial (Zerézima)")
+        layout.addWidget(btn_zerezima)
+        btn_votar = QPushButton("Votar")
+        layout.addWidget(btn_votar)
+        btn_relatorio = QPushButton("Relatório Final")
+        layout.addWidget(btn_relatorio)
+        btn_sair = QPushButton("Sair")
+        layout.addWidget(btn_sair)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    janela = Menu_urna()
+    janela.show()
+    sys.exit(app.exec())
