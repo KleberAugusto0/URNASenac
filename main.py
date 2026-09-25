114dfbc7aeda69f5b3ff5bfbe22b30be497b8117
JoaoCerbino <jonguicerbinonho@gmail.com>
2026-09-25 16:42:59 -0400

    Botões colocados funçoes com o comentario da logica

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PySide6.QtWidgets import QLabel, QVBoxLayout,QPushButton,QWidget,QApplicati
 from PySide6.QtCore import Qt
 import sys,os
 
-class Menu_urna(QWidget):
+class MenuUrna(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Menu")
@@ -23,20 +23,41 @@ class Menu_urna(QWidget):
 
         btn_votar = QPushButton("Votar")
         layout.addWidget(btn_votar,alignment=Qt.AlignmentFlag.AlignCenter)
+        btn_votar.setFixedSize(250,50)
     
 
         btn_relatorio = QPushButton("Relatório Final")
         layout.addWidget(btn_relatorio,alignment=Qt.AlignmentFlag.AlignCenter)
-
+        btn_relatorio.setFixedSize(250,50)
 
         btn_sair = QPushButton("Sair")
-        layout.addWidget(btn_sair,alignment=Qt.AlignmentFlag.AlignCenter) 
+        layout.addWidget(btn_sair,alignment=Qt.AlignmentFlag.AlignCenter)
+        btn_sair.setFixedSize(250,50) 
         layout.addStretch(1)
+
+
+
+
+        def zerezima():
+            verificacao_zerezima = True
+            #fazer logicoa pra chamar a tela
+            pass
+
+
+        def votar():
+            if verificacao_zerezima == True:
+                pass #fazer codigo para chamar a tela de votação
+            elif verificacao_zerezima == False:
+                pass#Fazer popup para fazer a zerezima primeiro
+
+
+        def relatorio():
+            pass
 
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    janela = Menu_urna()
+    janela = MenuUrna()
     janela.show()
     sys.exit(app.exec())
